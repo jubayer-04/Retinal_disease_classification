1c7cf9533241879cd35e336e9925ec39ef237328
Jubayer Hossain <jubayerhr@gmail.com>
2026-03-01 01:13:43 +0600

    Update app.py

--- app.py
+++ app.py
@@ -37,9 +37,14 @@ st.write("All classes are: ", class_names)
 if uploaded_file:
     image = Image.open(uploaded_file).resize((224, 224))
     left, center, right = st.columns([1,2,1])
+
     with center:
         st.image(image, caption="Uploaded Image")
-        predict_clicked = st.button("Predict")
+
+    # Create 3 small columns inside center to center button
+        b1, b2, b3 = st.columns([1,1,1])
+        with b2:
+            predict_clicked = st.button("Predict")
         
     
     img_array = np.array(image)
@@ -99,6 +104,7 @@ st.text(report_text)
 
 st.subheader("Model Description")
 st.text("We have worked with EfficientNetV2B3 model which is a convolutional neural network architecture that employs fused MBConv blocks and compound scaling to optimize accuracy–efficiency trade-offs while reducing training time. It leverages progressive learning and depth–width–resolution scaling to improve feature representation with fewer parameters. In this work, the model is fine-tuned via transfer learning on retinal fundus images for robust multiclass disease classification.")
+
 
 
 
