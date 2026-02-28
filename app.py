beae9038d74ef59d52e1fdb432ce6f1db1f40635
Jubayer Hossain <jubayerhr@gmail.com>
2026-03-01 00:51:40 +0600

    Update app.py

--- app.py
+++ app.py
@@ -87,11 +87,7 @@ st.text("Overall F1 Score: 0.985")
 st.text(report_text)
 
 st.subheader("Model Description")
-st.text("We have worked with EfficientNetV2B3 model which is a convolutional neural network architecture 
-that employs fused MBConv blocks and compound scaling to optimize accuracy–efficiency trade-offs 
-while reducing training time. 
-It leverages progressive learning and depth–width–resolution scaling to improve feature representation with fewer parameters. 
-In this work, the model is fine-tuned via transfer learning on retinal fundus images for robust multiclass disease classification.")
+st.text("We have worked with EfficientNetV2B3 model which is a convolutional neural network architecture that employs fused MBConv blocks and compound scaling to optimize accuracy–efficiency trade-offs while reducing training time. It leverages progressive learning and depth–width–resolution scaling to improve feature representation with fewer parameters. In this work, the model is fine-tuned via transfer learning on retinal fundus images for robust multiclass disease classification.")
 
 
 
