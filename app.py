f778aabd85a42393f475ff1be9fcb8f208c76b6a
Jubayer Hossain <jubayerhr@gmail.com>
2026-03-01 01:20:09 +0600

    Update app.py

--- app.py
+++ app.py
@@ -69,13 +69,13 @@ if uploaded_file:
     
         fig = plt.figure(figsize=(3.5, 2.2))  # smaller figure
 
-        plt.bar(class_names, probabilitites * 100, width=0.5)
+        plt.bar(class_names, probabilitites * 100, width=1)
         
-        plt.xticks(rotation=35, fontsize=7)
-        plt.yticks(fontsize=7)
+        plt.xticks(rotation=45, fontsize=5)
+        plt.yticks(fontsize=5)
         
-        plt.xlabel("Classes", fontsize=8)
-        plt.ylabel("Confidence (%)", fontsize=8)
+        plt.xlabel("Classes", fontsize=5)
+        plt.ylabel("Confidence (%)", fontsize=5)
         
         plt.ylim(0, 100)
         
@@ -108,6 +108,7 @@ st.text(report_text)
 
 st.subheader("Model Description")
 st.text("We have worked with EfficientNetV2B3 model which is a convolutional neural network architecture that employs fused MBConv blocks and compound scaling to optimize accuracy–efficiency trade-offs while reducing training time. It leverages progressive learning and depth–width–resolution scaling to improve feature representation with fewer parameters. In this work, the model is fine-tuned via transfer learning on retinal fundus images for robust multiclass disease classification.")
+
 
 
 
