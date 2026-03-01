ca55d63d0b9970760e37360d302dbfb8a4a0ad30
Jubayer Hossain <jubayerhr@gmail.com>
2026-03-01 14:47:24 +0600

    Update app.py

--- app.py
+++ app.py
@@ -192,10 +192,10 @@ st.text(report_text)
 st.subheader("Model Description")
 st.text("We have worked with EfficientNetV2B3 model which is a convolutional neural network architecture that employs fused MBConv blocks and compound scaling to optimize accuracy–efficiency trade-offs while reducing training time. It leverages progressive learning and depth–width–resolution scaling to improve feature representation with fewer parameters. In this work, the model is fine-tuned via transfer learning on retinal fundus images for robust multiclass disease classification.")
 
-"""import tempfile
-from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Image as RLImage
-from reportlab.lib.styles import getSampleStyleSheet
-from reportlab.lib.units import inch"""
+#import tempfile
+#from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Image as RLImage
+#from reportlab.lib.styles import getSampleStyleSheet
+#from reportlab.lib.units import inch
 
 
 # ------------------ Suggestion Logic ------------------
@@ -310,6 +310,7 @@ if "predicted_class" in st.session_state:
             file_name=f"{patient_name}_report.pdf",
             mime="application/pdf"
         )
+
 
 
 
