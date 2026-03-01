b987d275611d0a70f9859ea8c9d34344325eef98
Jubayer Hossain <jubayerhr@gmail.com>
2026-03-02 02:00:05 +0600

    Update app.py

--- app.py
+++ app.py
@@ -196,9 +196,9 @@ with st.sidebar:
         st.markdown("*Jubayer Hossian*")
         st.markdown(" Computer Science & Engineering")
         st.markdown("Daffodil International University")
-        st.markdown("[📧](mailto:jubayerhossain.cse@gmail.com)")
-        st.markdown("[💼](https://www.linkedin.com/in/jubayer-hossain-628b92292/)")
-        st.markdown("[💻](https://github.com/jubayer-04)")
+        st.markdown("[📧](mailto:jubayerhossain.cse@gmail.com)", "[💼](https://www.linkedin.com/in/jubayer-hossain-628b92292/)", "[💻](https://github.com/jubayer-04)")
+        #st.markdown("[💼](https://www.linkedin.com/in/jubayer-hossain-628b92292/)")
+        #st.markdown("[💻](https://github.com/jubayer-04)")
         st.markdown("---")
         st.markdown("*Nazia Sultana Marjan*")
         st.markdown(" Computer Science & Engineering")
@@ -480,6 +480,7 @@ st.markdown(f"""
 """, unsafe_allow_html=True)
 
 st.markdown("<br><br><br><br>", unsafe_allow_html=True)
+
 
 
 
